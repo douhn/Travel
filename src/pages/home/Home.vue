337930c9392ca7522c18c6d6f4a24228cc486c4e
douhn <douhn@foxmail.com>
2018-08-12 18:42:23 +0800

    use keep-alive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,15 +27,19 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get(`/api/index.json?city=${this.city}`)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -49,7 +54,16 @@ export default {
     }
   },
   mounted () {
-    this.getHomeInfo()
+    this.lastCity = this.city
+    this.getHomeInfo()// 页面初次被加载
+  },
+  activated () {
+    // 该钩子在服务器端渲染期间不被调用。
+    // 页面重新被渲染（keep-alive 组件激活时调用）
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
